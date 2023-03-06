b255e908cf48422a7a02e2c17f6ec287bae7bbde
Akhil Raj <akhilrajofindia@gmail.com>
2023-03-06 22:11:51 +0530

    sixth commit

---
+++ 1714A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int tc; cin>>tc;
+    while(tc--){
+        int n,h,m; 
+        cin>>n>>h>>m;
+        int a=h*60+m;
+        int x=1440;
+        for (int i = 0; i < n; i++)
+        {
+            int h,m; 
+            cin>>h>>m;
+            int b=h*60+m;
+            if(a>b){
+                b+=1440;
+            }
+            x=min(x,b-a);
+        }
+        // while(n--){
+        //     int h,m; 
+        //     cin>>h>>m;
+        //     int b=h*60+m;
+        //     if(a>b){
+        //         b+=1440;
+        //     }
+        //     x=min(x,b-a);
+        // }
+        cout<<x/60<<" "<<x%60<<endl;
+    }
+
+    return 0;
+}
