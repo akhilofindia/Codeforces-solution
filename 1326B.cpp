9c1bcd6bdb86e1e34625cd15e948c1d9e528aa43
Akhil Raj <akhilrajofindia@gmail.com>
2023-03-17 23:53:09 +0530

    solution

---
+++ 1326B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) {
+       cin>>arr[i];
+    }
+    cout<<arr[0]<<" ";
+    int ans=arr[0];
+    for(int i=1;i<n;i++) {
+        if(arr[i]<0){
+           cout<<ans+arr[i]<<" ";
+        }else{
+           cout<<ans+arr[i]<<" ";
+           ans=ans+arr[i];
+        }
+    }
+
+	return 0;
+}
