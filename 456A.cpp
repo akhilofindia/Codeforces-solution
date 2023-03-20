65b308d79d31a4a67e15e8cb6269f1827c21103c
Akhil Raj <akhilrajofindia@gmail.com>
2023-03-20 21:31:18 +0530

    solution

---
+++ 456A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+    int count=0;
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        if(a!=b){
+    	    count=1;
+    	    break;
+	    }
+    }
+	if (count>0){
+    	cout<<"Happy Alex"<<endl; 
+	}else
+	cout<<"Poor Alex"<<endl;
+	
+
+	return 0;
+}
