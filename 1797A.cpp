3d4b4b9aa8e0ebfdaf169c4b3e884cf1ac7e804f
Akhil Raj <akhilrajofindia@gmail.com>
2023-04-09 22:33:56 +0530

    solution

--- 1797A.cpp
+++ 1797A.cpp
@@ -7,14 +7,13 @@ int main(){
 		int n,m;
 		cin>>n>>m;
 		int a,b,c,d; cin>>a>>b>>c>>d;
-		if((a==1&&b==1) || (c==1&&d==1) ||(a==n&&b==m) || (c==n&&d==m) ||(a==1&&b==m) ||(c==1&&d==m) || (a==n&&b==1)||(c==n&&d==1)){
+		if((a==1&&b==1) || (c==1&&d==1) ||(a==n&&b==m) || (c==n&&d==m) ||(a==1&&b==m) ||(c==1&&d==m) || (a==n&&b==1) || (c==n&&d==1)){
             cout<<2<<endl;
         }else if(a==1||a==n||c==1||c==n||b==1||b==m||d==1||d==m){
             cout<<3<<endl;
         }else{
             cout<<4<<endl;
         }
-
 	}
 
 	return 0;
