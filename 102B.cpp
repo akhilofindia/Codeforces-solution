5f7585ea122ff60334c2a276fc2c77cbace1165d
Akhil Raj <akhilrajofindia@gmail.com>
2023-04-16 22:44:26 +0530

    solution

---
+++ 102B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s;
+    cin >> s;
+    int count = 0;
+    while (s.size()!=1){
+        int ans = 0;
+        for (int i = 0; i < s.size(); i++){
+            ans += s[i] - '0';
+        }
+        s=to_string(ans);
+        count++;
+    }
+    cout << count << endl;
+
+	return 0;
+}
