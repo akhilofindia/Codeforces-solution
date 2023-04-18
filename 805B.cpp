ead337f6e0e2db6767c50f623244ce452c3feb04
Akhil Raj <akhilrajofindia@gmail.com>
2023-04-18 23:31:43 +0530

    solution

---
+++ 805B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	for(int i=0;i<n;i++){
+		if(i%4==0||i%4==1){
+			cout << 'a';
+		}else{
+			cout << 'b';
+		}
+		
+	}
+
+	return 0;
+}
