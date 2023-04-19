dc0e8ec364eccbaa43740f18c6e8e7787ea34c06
Akhil Raj <akhilrajofindia@gmail.com>
2023-04-19 22:04:36 +0530

    solution

--- 805B.cpp
+++ 805B.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 int main(){
-	int n;
-	cin >> n;
-	for(int i=0;i<n;i++){
+	int a;
+	cin >>a;
+	for(int i = 0; i < a ;i++){
 		if(i%4==0||i%4==1){
-			cout << 'a';
+			cout<<'a';
 		}else{
-			cout << 'b';
-		}
-		
+			cout<<'b';
+		}	
 	}
 
 	return 0;
