a5e50322f23b06bad05f80d8e146076cea95390a
Akhil Raj <akhilrajofindia@gmail.com>
2023-05-05 21:53:55 +0530

    solution

--- 1389A.cpp
+++ 1389A.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main(){
 	int tc; cin>>tc;
 	while(tc--){
-		int a, b; 
-		cin >> a >> b;
+		int a,b; 
+		cin>>a>>b;
     	if (b >= 2*a){
-        	cout << a << " " << 2*a << endl;
+        	cout<<a<< " "<<2*a<<endl;
 	    }
     	else{
-        	cout << -1 << " " << -1 << endl;
+        	cout<<-1<<" "<<-1<< endl;
     	}
 	}
 
---
+++ 1826A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int tc; cin>>tc;
+	while(tc--){
+		int n;
+        cin>>n;
+		vector <int> a(n);
+		for (int i = 0; i < n; i++)
+		{
+			cin>>a[i];
+		}
+		sort(a.begin(),a.end());
+		int ans=0,b=0;
+		for(int i=0;i<=n;i++){
+	    	int count=0,count2=0;
+            for (int j = 0; j < n; j++)
+            {
+            	if (a[j]>i)
+            	{
+            		count++;
+            	}else
+            	    count2++;
+            }
+			if(count==i){
+           		cout<<i<<endl;
+           		b=1;
+           		break;
+			}
+		}
+		if (b==0)
+		{
+			cout<<-1<<endl;
+		}
+	}
+
+	return 0;
+}
