47d29809d4a85e24c52d647a54c1f372a6bb7c0c
Akhil Raj <akhilrajofindia@gmail.com>
2023-05-10 21:27:35 +0530

    solution

---
+++ 798A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,count=0;
+	string s;
+	cin>>s;
+	n=s.length();
+	for(int i=0; i < n/2; i++){
+		if(s[i]!=s[n-1-i]){
+			count++;
+		}
+	}
+	if(count==1){
+		cout<<"YES"<<endl;
+	}
+	else {
+		if(n%2==1 && count==0){
+			cout<<"YES"<<endl;
+		}
+		else {
+			cout<<"NO"<<endl;
+		}
+	}
+
+	return 0;
+}
