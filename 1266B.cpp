7016885807295ab3cdb4042f04444f0c06fc2aa4
Akhil Raj <akhilrajofindia@gmail.com>
2023-05-17 20:56:44 +0530

    solution

---
+++ 1266B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int tc; cin>>tc;
+	while(tc--){
+		int n;
+        cin>>n;
+        int a=n%14;
+        if(a<7 && a!=0 && n>14){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+	}
+
+	return 0;
+}
