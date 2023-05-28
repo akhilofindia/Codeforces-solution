0fe738774a3bb4174d51bd3c3294761f7bc6f010
Akhil Raj <akhilrajofindia@gmail.com>
2023-05-28 23:07:41 +0530

    solution

---
+++ 1831A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int tc; cin>>tc;
+	while(tc--){
+		int n; cin>>n;
+		int arr[n];
+		for (int i = 0; i < n; i++)
+		{
+			cin>>arr[i];
+		}
+		int b[n];
+		for (int i = 0; i < n; i++)
+		{
+			b[i]=n+1-arr[i];
+			cout<<b[i]<<" ";
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
