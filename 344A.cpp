c432bfe9812bb2f66f257d122268df8b15846034
Akhil Raj <akhilrajofindia@gmail.com>
2023-05-29 22:16:56 +0530

    solution

---
+++ 344A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// int tc; cin>>tc;
+	// while(tc--){
+		int n, count= 1;
+		cin>>n;
+		int arr[n];
+		for (int i = 0; i < n; i++){
+			cin >> arr[i];
+		}
+		for (int i = 1; i < n; i++)
+		{
+			if (arr[i] != arr[i-1]){
+				count++;
+			}
+		}
+		cout<<count<<endl;
+	
+
+	return 0;
+}
