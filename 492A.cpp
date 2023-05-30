39023b14bbb6d9ca09821e6c079cdd94ad96b348
Akhil Raj <akhilrajofindia@gmail.com>
2023-05-30 21:17:41 +0530

    solution

---
+++ 492A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int tc; cin>>tc;
+	while(tc--){
+		int n,k; 
+		cin>>n>>k;
+    	string s; cin>>s ;
+    	string t=s;
+    	reverse (t.begin(),t.end());
+    	if (s == t || k == 0 ){
+    	    cout << 1 << endl ;
+    	}
+    	else{
+    	    cout<<2<<endl ;
+    	}
+	}
+
+	return 0;
+}
