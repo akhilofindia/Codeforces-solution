4d79afb3d7d6c84c6461919307d47e26bb737383
Akhil Raj <akhilrajofindia@gmail.com>
2023-06-30 23:46:44 +0530

    solution

--- 1790D.cpp
+++ 1790D.cpp
@@ -4,23 +4,25 @@ using namespace std;
 int main(){
 	int tc;cin>>tc;
 	while(tc--){
-		int n;cin>>n;
-		int a[n];
-		map<int,int>mp;
-		set<int>st;
-		for (int i = 0; i < n; i++)
-		{
-			cin>>a[i];
-			mp[a[i]]++; 
-			st.insert(a[i]);
-		}
-		for (int i = 0; i < mp.size(); i++)
-		{
-			if (st.count(a[i]))
-			{
-				mp[a[i]]--;
-			}
-		}
+		int n;
+        cin>>n;
+        set<int> st;
+        map<int, int> mp;
+        for (int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            st.insert(a);
+            st.insert(a+1);
+            mp[a]++;
+        }
+        int lastnum  = 0;
+        int ans = 0;
+        for (auto it : st){
+            int c = mp[it];
+            ans+=max(0,c-lastnum);
+            lastnum = c;
+        }
+        cout<<ans<<"\n";
 		
 	}
 
