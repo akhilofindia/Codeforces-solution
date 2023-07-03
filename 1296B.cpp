a6136a3d94895ef6777ef6a16b2fb69f3e7d0c26
Akhil Raj <akhilrajofindia@gmail.com>
2023-07-03 19:05:16 +0530

    solution

---
+++ 1296B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int tc;cin>>tc;
+	while(tc--){
+		int a;cin>>a;
+		cout<<a+(a-1)/9<<endl;
+	}
+
+	return 0;
+}
--- 1624B.cpp
+++ 1624B.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main(){
 	int tc;cin>>tc;
 	while(tc--){
-		int a,b,c;
+		long long a,b,c;
         cin>>a>>b>>c;
-        if(b-a==c-b){
+        if(b-a == c-b){
             cout<<"YES"<<endl;
         }
         else if(abs(a+c)%(2*b)==0){
             cout<<"YES"<<endl;
         }
-        else if(((2*b-c)>0)&&(2*b-c)%a==0){
+        else if(((2*b-c)>0) && (2*b-c)%a==0){
             cout<<"YES"<<endl;
         }
-        else if(((2*b-a)>0)&&((2*b-a)%c==0)){
+        else if(((2*b-a)>0) && ((2*b-a)%c==0)){
             cout<<"YES"<<endl;
         }
         else{
