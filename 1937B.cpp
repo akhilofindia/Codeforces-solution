d7052433c3085f6c7ffc5cfc3f8907047bded175
akhilofindia <111959737+akhilofindia@users.noreply.github.com>
2025-01-29 22:13:11 +0530

    Add files via upload

--- 1937B.cpp
+++ 1937B.cpp
@@ -5,59 +5,36 @@ using namespace std;
 #define cy cout<<"YES"<<endl
 #define cn cout<<"NO"<<endl
 #define disp(a,x) for(auto x:a)cout<<x<<" ";
+#define ce cout<<endl;
+#define int long long
 
+template<typename T>void displayVec(vector<T> a){for(int i=0;i<a.size();i++) cout<<a[i]<<" ";cout<<endl;}
+template<typename T>void displayArr(T a[], int n){for(int i=0;i<n;i++) cout<<a[i]<<" ";cout<<endl;}
+template<typename T>void displayAdj(vector<T> adj[], int n){for(int i=0;i<=n;i++){cout<<i<<"->";displayVec(adj[i]);}}
 template<typename T1, typename T2>void displayMap(map<T1,T2>mp){for(auto it : mp){cout<<it.first<<"->"<<it.second<<endl;}cout<<endl;}
+template<typename T>void displaySet(set<T>st){for(auto it:st){cout<<it<<" ";}cout<<endl;}
+template<typename T>void displayQueue(queue<T> q){while(!q.empty()){cout<<q.front()<<" ";q.pop();}cout<<endl;}
+template<typename T>void displayPQ(priority_queue<T> pq){while(!pq.empty()){cout<<pq.top()<<" ";pq.pop();}cout<<endl;}
 
-int main(){
-	int tc;cin>>tc;
-	while(tc--){
-		int n;cin>>n;
-		string s1,s2;
-		cin>>s1>>s2;
-		int flag=0;
-		string s3="";
-		for (int i = 0; i < n; i++)
-		{
-			if (flag==0)
-			{
-				if (s1[i]=='0')
-				{
-					s3+='0';
-				}else if(s2[i]=='0'){
-					s3+='0';
-					flag=1;
-				}else{
-					if (s1[i]=='1')
-					{
-						s3+='1';
-					}else{
-						s3+='1';
-						flag=1;
-					}
-
-				}
-			}else{
-				if (s2[i]=='0' )
-				{
-					s3+='0';
-				}else if (s1[i]=='0')
-				{
-					s3+='0';
-					flag=0;
-				}else{
-					if (s2[i]=='1')
-					{
-						s3+='1';
-					}else{
-						s3+='1';
-						flag=0;
-					}
-				}
-			}
-		}
-		s3+=s2[n-1];
-		cout<<s3<<endl;
-	}
-
-	return 0;
+signed main(){
+    int tc; cin >> tc;;
+    while (tc--) {
+      int n; cin>>n;
+      string s,t;cin>>s>>t;
+      int ind=1;
+      for (int i = 1; i < n; i++) 
+      {
+          if (s[i]==t[i-1]) {
+            ind++;
+          }else{
+            if (s[i] == '1') {
+              break;
+            }
+            ind = 1;
+            t[i-1]=s[i];
+          }
+      }
+      cout<<s[0]<<t<<endl<<ind<<endl;
+    }
+    return 0;
 }
